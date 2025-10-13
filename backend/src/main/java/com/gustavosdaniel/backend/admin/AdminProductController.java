e137e66300628f9ca38b3f18300602f738167986
GustavoSDaniel <gustavosdaniel@hotmail.com>
2025-10-13 06:44:55 -0300

    feat: adicionando metodo no controller admproduct de update e delete

--- backend/src/main/java/com/gustavosdaniel/backend/admin/AdminProductController.java
+++ backend/src/main/java/com/gustavosdaniel/backend/admin/AdminProductController.java
@@ -1,19 +1,13 @@
 package com.gustavosdaniel.backend.admin;
 
 import com.gustavosdaniel.backend.image.ErrorValidateImage;
-import com.gustavosdaniel.backend.product.ExceptionProductNameExists;
-import com.gustavosdaniel.backend.product.ProductCreatedRequest;
-import com.gustavosdaniel.backend.product.ProductResponse;
-import com.gustavosdaniel.backend.product.ProductService;
+import com.gustavosdaniel.backend.product.*;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -39,6 +33,28 @@ public class AdminProductController {
                 createdProduct(productCreatedRequest, imageFile);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(productCreatedResponse);
+    }
+
+    @PutMapping(value = "/{id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @Operation(summary = "Atualizando Produto")
+    public ResponseEntity<ProductResponse> updateProduct(
+            @PathVariable String id,
+            @RequestPart(value = "productImage", required = false) MultipartFile productImage,
+             @Valid ProductUpdateRequest productUpdateRequest)
+            throws IOException, ErrorValidateImage, ProductNameAlreadyExistsException
+    {
+
+        ProductResponse updateProduct = productService.
+                updateProduct(productImage, id, productUpdateRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Deletando Produto")
+    public ResponseEntity<Valid> deleteProduct(@PathVariable String id){
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
